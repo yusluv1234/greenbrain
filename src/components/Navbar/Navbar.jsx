ad7114f8e375d818c785d0f87a44ec3b63900798
yusluv1234 <yusluv124@gamil.com>
2020-11-30 09:22:41 +0100

    feat: add neccessary dependencies

---
+++ src/components/Navbar/Navbar.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+
+import './navbar.css';
+
+const Navbar = () => {
+    return (
+        <div className='navbar'>
+            <div className='nav-logo'> greenbrain </div>
+            <input className='inpute' placeholder='Search' />
+            <div className='header-icons'>
+                <h2>gg</h2>
+                <h2>ff</h2>
+                <h2>ff</h2>
+            </div>
+        </div>
+    )
+}
+
+export default Navbar;

8f5c2ac655f549591f5e0a12b1d6b9e3e308dd42
yusluv1234 <yusluv124@gamil.com>
2020-11-30 17:13:17 +0100

    sigIn

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import HomePages from './components/pages/home-pages/HomePages';
+import SigninPages from './components/pages/sign-in-pages/SigninPages';
 
 import './App.css';
 
@@ -13,6 +14,7 @@ function App() {
       <Router>
       <Switch>
       <Route exact path="/" component={HomePages} />
+      <Route path="/signIn" component={SigninPages} />
       </Switch>
       </Router>
     </div>
---
+++ src/components/pages/sign-in-pages/SigninPages.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import SignIn from '../../sign-in/SignIn';
+
+const SigninPages = () => {
+    return (
+        <div className='sign-in-pages'>
+            <SignIn />
+        </div>
+    )
+}
+
+export default SigninPages;
